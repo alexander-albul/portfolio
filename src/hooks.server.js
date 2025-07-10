5a881d0cc4462ad3819a48cc36096453e450dcef
alexander-albul <ololouololou@gmail.com>
2025-07-11 00:19:38 +0300

    paths

---
+++ src/hooks.server.js
@@ -0,0 +1,9 @@
+// src/hooks.server.js
+import { redirect } from '@sveltejs/kit';
+
+export async function handle({ event, resolve }) {
+  if (event.url.pathname === '/') {
+    redirect(307, '/works/pages');
+  }
+  return resolve(event);
+}
